a4bd6ac6cbaae3c705e96dbc074cadf8cf751837
gandalfmagic <gandalfmagic@libero.it>
2021-05-24 09:22:04 +0200

    feat: prepare the environemnt fom the entrypoint using root user

--- pg-12/entrypoint.sh
+++ pg-12/entrypoint.sh
@@ -6,17 +6,13 @@ set -f
 set -o pipefail
 set -u
 
-echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Preparing the SSH environment"
-mkdir -p ${HOME}/.ssh && chmod 700 ${HOME}/.ssh
-ssh-keyscan ${PGBR_REPO_HOST} > ${HOME}/.ssh/known_hosts 2>/dev/null && chmod 600 ${HOME}/.ssh/known_hosts
-echo "${PGBR_SSH_USER_KEY}" > ${HOME}/.ssh/id_rsa && chmod 600 ${HOME}/.ssh/id_rsa
+if [ ! -f /var/lib/postgresql/12/main/PG_VERSION ]; then
+  mkdir -p /var/lib/postgresql/12/main
+  chown -R postgres:postgres /var/lib/postgresql/12
+fi
 
-echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Restore from ${PGBR_REPO_HOST} started"
-touch /tmp/pgbackrest_empty.conf
-pgbackrest restore --config=/tmp/pgbackrest_empty.conf --delta --log-level-console=error --log-level-file=error --pg1-path=/var/lib/postgresql/12/main --process-max=${PGBR_PROCESS_MAX} --recovery-option=recovery_target_action=promote --repo1-host="${PGBR_REPO_HOST}" --repo1-host-user="${PGBR_SSH_USER:-pgbackrest}" --repo1-path="${PGBR_REPO_PATH}" --stanza="${PGBR_STANZA}" --type=immediate
-echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Restore ended"
+if [ -f /var/lib/postgresql/12/main/postmaster.pid ]; then
+  rm /var/lib/postgresql/12/main/postmaster.pid
+fi
 
-echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: PostgreSQL server started"
-sed -r -i "s/(#?)listen_addresses = (.*)$/listen_addresses = '*'/" /etc/postgresql/12/main/postgresql.conf
-sed -r -i "s/(#?)max_connections = (.*)/max_connections = ${PGBR_PG_MAX_CONNECTIONS:-100}/" /etc/postgresql/12/main/postgresql.conf
-/usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf
+su postgres -c ./restore.sh
---
+++ pg-12/restore.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -C
+set -e
+set -f
+set -o pipefail
+set -u
+
+echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Preparing the SSH environment"
+mkdir -p ${HOME}/.ssh && chmod 700 ${HOME}/.ssh
+ssh-keyscan ${PGBR_REPO_HOST} > ${HOME}/.ssh/known_hosts 2>/dev/null && chmod 600 ${HOME}/.ssh/known_hosts
+echo "${PGBR_SSH_USER_KEY}" > ${HOME}/.ssh/id_rsa && chmod 600 ${HOME}/.ssh/id_rsa
+
+echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Restore from ${PGBR_REPO_HOST} started"
+touch /tmp/pgbackrest_empty.conf
+pgbackrest restore --config=/tmp/pgbackrest_empty.conf --delta --log-level-console=error --log-level-file=error --pg1-path=/var/lib/postgresql/12/main --process-max=${PGBR_PROCESS_MAX} --recovery-option=recovery_target_action=promote --repo1-host="${PGBR_REPO_HOST}" --repo1-host-user="${PGBR_SSH_USER:-pgbackrest}" --repo1-path="${PGBR_REPO_PATH}" --stanza="${PGBR_STANZA}" --type=immediate
+echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Restore ended"
+
+echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: PostgreSQL server started"
+sed -r -i "s/(#?)listen_addresses = (.*)$/listen_addresses = '*'/" /etc/postgresql/12/main/postgresql.conf
+sed -r -i "s/(#?)max_connections = (.*)/max_connections = ${PGBR_PG_MAX_CONNECTIONS:-100}/" /etc/postgresql/12/main/postgresql.conf
+/usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf

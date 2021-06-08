1c967274cec2d0ae0ad5dc5f8a458e243485e0ec
gandalfmagic <gandalfmagic@libero.it>
2021-06-08 16:07:46 +0200

    Build the ssh environment with root permissions

--- pg-11/entrypoint.sh
+++ pg-11/entrypoint.sh
@@ -15,4 +15,16 @@ if [ -f /var/lib/postgresql/11/main/postmaster.pid ]; then
   rm /var/lib/postgresql/11/main/postmaster.pid
 fi
 
+echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Preparing the SSH environment"
+SSH_DIR=/var/lib/postgresql/.ssh
+(mkdir -p ${SSH_DIR} && chmod 700 ${SSH_DIR} && chown postgres:postgres ${SSH_DIR}) || true
+echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Add known host"
+ssh-keyscan ${PGBR_REPO_HOST} > ${SSH_DIR}/known_hosts 2>/dev/null
+chmod 600 ${SSH_DIR}/known_hosts
+chown postgres:postgres ${SSH_DIR}/known_hosts
+if [ -f ${SSH_DIR}/id_rsa ]; then
+  echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Change id_rsa file permissions"
+  chmod 644 ${SSH_DIR}/id_rsa
+fi
+
 su postgres -c ./restore.sh
--- pg-13/restore.sh
+++ pg-13/restore.sh
@@ -6,11 +6,6 @@ set -f
 set -o pipefail
 set -u
 
-echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Preparing the SSH environment"
-mkdir -p ${HOME}/.ssh && chmod 700 ${HOME}/.ssh
-ssh-keyscan ${PGBR_REPO_HOST} > ${HOME}/.ssh/known_hosts 2>/dev/null && chmod 600 ${HOME}/.ssh/known_hosts
-echo "${PGBR_SSH_USER_KEY}" > ${HOME}/.ssh/id_rsa && chmod 600 ${HOME}/.ssh/id_rsa
-
 echo "$(date +'%Y-%m-%d %H:%M:%S %Z') --- LOG: Restore from ${PGBR_REPO_HOST} started"
 touch /tmp/pgbackrest_empty.conf
 pgbackrest restore --config=/tmp/pgbackrest_empty.conf --delta --log-level-console=error --log-level-file=error --pg1-path=/var/lib/postgresql/13/main --process-max=${PGBR_PROCESS_MAX} --recovery-option=recovery_target_action=promote --repo1-host="${PGBR_REPO_HOST}" --repo1-host-user="${PGBR_SSH_USER:-pgbackrest}" --repo1-path="${PGBR_REPO_PATH}" --stanza="${PGBR_STANZA}" --type=immediate
